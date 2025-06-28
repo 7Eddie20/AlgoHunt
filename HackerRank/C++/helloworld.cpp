db03e60129f60fce1dacaf0b20e638d1d5114357
7Eddie20 <aditya.edn@gmail.com>
2025-06-28 21:40:13 +0530

    28-6-25

---
+++ HackerRank/C++/helloworld.cpp
@@ -0,0 +1,20 @@
+/*
+🔗 Problem Link:
+https://www.hackerrank.com/challenges/cpp-hello-world/problem
+
+📄 Problem Summary:
+- print a simple hello world.
+
+🧠 Approach:
+- Use cout to print
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,28 @@
+/*
+🔗 Problem Link:
+https://www.hackerrank.com/challenges/example-problem-name
+
+📄 Problem Summary:
+Given two integers, sum them and print the result.
+
+🧠 Approach:
+- Use `cin` to take input
+- Perform simple addition
+- Print using `cout`
+
+💡 Notes:
+- Practicing basic input/output
+- Handles integer constraints only
+
+👨‍💻 Status: ✅ Solved & Tested
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
